d453035f39e35318468706413fc2332ccdae2592
LuigiP2404 <71146060+LuigiP2404@users.noreply.github.com>
2021-08-13 09:24:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            tasks: [],
+            newTask: '',
+            show: true
+        };
+    },
+    computed: {
+        fraseBottone() {
+            if (this.show) {
+                return 'Nascondi Lista';
+            }
+            else {
+                return 'Mostra Lista';
+            }
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.newTask);
+            this.newTask = '';
+        },
+        toggleList() {
+            this.show = !this.show;
+        },
+        removeTask(id) {
+            this.tasks.splice(id, 1);
+        }
+    }
+});
+app.mount('#assignment');
